c09db9de6aa2d821aa7e0eda26c514c9426942ff
keisuke <kinta.business@gmail.com>
2023-03-16 20:49:09 +0900

    test commit

--- scr/index.js
+++ scr/index.js
@@ -13,6 +13,15 @@ const onClickAdd = () => {
     const li = document.createElement("li");
     li.innerText = inputText;
 
+    // button(完了)タグ生成
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+
+    // button(削除)タグ生成
+    const deleteButton = document.createElement("button");
+    completeButton.innerText = "削除";
+    
+
     // divタグのs子要素に各要素を設定
     div.appendChild(li);
 
